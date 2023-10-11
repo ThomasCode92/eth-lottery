260c6b5a706cf4074e7de58d7721c69933922dc3
ThomasCode92 <thomascode92@hotmail.com>
2023-10-11 08:07:39 +0200

    Web3 Setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,17 @@
+import web3 from './utils/web3';
+
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const listAccounts = async () => {
+      return await web3.eth.getAccounts();
+    };
+
+    listAccounts().then(console.log);
+  }, []);
+
   return <div></div>;
 }
 

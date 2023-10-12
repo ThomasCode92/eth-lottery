72ddf9e70e6db1db4b88f8442c0d2a1a3edce7ee
ThomasCode92 <thomascode92@hotmail.com>
2023-10-12 07:50:41 +0200

    Accessing more Properties

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,18 +7,28 @@ import './App.css';
 
 function App() {
   const [manager, setManager] = useState('');
+  const [players, setPlayers] = useState([]);
+  const [balance, setBalance] = useState('');
 
   useEffect(() => {
-    const fetchManager = async () => {
+    const fetchData = async () => {
       try {
-        const managerAddress = await lottery.methods.manager().call();
+        const [managerAddress, playerAddresses, lotteryBalance] =
+          await Promise.all([
+            lottery.methods.manager().call(),
+            lottery.methods.getPlayers().call(),
+            web3.eth.getBalance(lottery.options.address),
+          ]);
+
         setManager(managerAddress);
+        setPlayers(playerAddresses);
+        setBalance(lotteryBalance);
       } catch (error) {
-        console.error('Error fetching manager:', error);
+        console.error('Error fetching contract data:', error);
       }
     };
 
-    fetchManager();
+    fetchData();
   }, []);
 
   return (
@@ -30,6 +40,10 @@ function App() {
         <section>
           <h2>Lottery Contract</h2>
           <p>This contract is managed by {manager}</p>
+          <p>
+            There are currently {players.length} people entered, competing to
+            win {web3.utils.fromWei(balance, 'ether')} ether!
+          </p>
         </section>
       </main>
     </Fragment>
